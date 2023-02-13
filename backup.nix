ea6234811f087b0db6849d9e5c350510ac78fb7a
Thijs Raymakers <thijs@raymakers.nl>
2023-02-14 00:15:40 +0100

    Use different format for secrets

--- backup.nix
+++ backup.nix
@@ -1,19 +1,12 @@
-{... }: {
-  imports = [ <sops-nix/modules/sops> ];
+{ sops-nix, ... }: {
   # This will add secrets.yml to the nix store
   # You can avoid this by adding a string to the full path instead, i.e.
-  # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
-  sops.defaultSopsFile = ./secrets/odysseus/restic_credentials.env;
+  sops.defaultSopsFile = ./secrets/odysseus/secrets.env;
   # This will automatically import SSH keys as age keys
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   # This is using an age key that is expected to already be in the filesystem
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   # This will generate a new key if the key specified above does not exist
   sops.age.generateKey = true;
-
-    # services = {
-    #   restic.backups.backblaze = {
-
-    #   };
-    # };
+  sops.secrets."restic_credentials" = {};
 }
