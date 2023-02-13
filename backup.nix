15d75909cbbee1193a4218b977d58936f2dc012c
Thijs Raymakers <thijs@raymakers.nl>
2023-02-14 00:32:43 +0100

    Add restic

--- backup.nix
+++ backup.nix
@@ -8,5 +8,22 @@
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   # This will generate a new key if the key specified above does not exist
   sops.age.generateKey = true;
-  sops.secrets."restic_credentials" = {};
+  sops.secrets."restic/api_credentials" = {};
+  sops.secrets."restic/repository" = {};
+  sops.secrets."restic/password" = {};
+
+  services.restic.backups.backblaze = {
+    s3CredentialsFile = "/run/secrets/restic/api_credentials";
+    repositoryFile = "/run/secrets/restic/repository";
+    passwordFile = "/run/secrets/restic/password";
+    initialize = true;
+    paths = [
+      "/home"
+    ];
+    user = "root";
+    timerConfig = {
+      OnCalendar = "00:05";
+      RandomizedDelaySec = "5h";
+    };
+  };
 }
