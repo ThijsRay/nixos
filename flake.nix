02ebd95386768d9da8bd0b3c7604d834776dc5fb
Thijs Raymakers <thijs@raymakers.nl>
2023-02-13 23:22:36 +0100

    Add sops for managing secrets

--- flake.nix
+++ flake.nix
@@ -1,11 +1,12 @@
 {
   inputs = {
-	nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
   description = "A very basic flake";
 
-  outputs = inputs@{ self, nixpkgs }: {
+  outputs = inputs@{ self, nixpkgs, sops-nix }: {
 	nixosConfigurations = {
 		odysseus = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
@@ -14,6 +15,7 @@
 				./devtools.nix
 				./configuration.nix
 				./hardware-configuration.nix
+        sops-nix.nixosModules.sops
 			];
 		};
 	};
