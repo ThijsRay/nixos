ac36c5452c2e378042db1caa6e632013ce3a0309
Thijs Raymakers <thijs@raymakers.nl>
2023-02-16 22:35:02 +0100

    Add more info about ipv6 and the firewall

--- networking.nix
+++ networking.nix
@@ -1,5 +1,13 @@
 { ... }: {
   networking = {
+    firewall = {
+      allowedTCPPorts = [ 22 80 443 ];
+    };
+    enableIPv6 = true;
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "ens18";
+    };
     interfaces.ens18 = {
       ipv6.addresses = [
         {
