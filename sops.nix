aa6cea0b5791dd5fb6512937530eb94657538599
Thijs Raymakers <thijs@raymakers.nl>
2023-02-16 21:14:27 +0100

    Split sops into separate file

---
+++ sops.nix
@@ -0,0 +1,11 @@
+{ sops-nix, ... }: {
+  # This will add secrets.yml to the nix store
+  # You can avoid this by adding a string to the full path instead, i.e.
+  sops.defaultSopsFile = ./secrets/odysseus/secrets.yaml;
+  # This will automatically import SSH keys as age keys
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  # This is using an age key that is expected to already be in the filesystem
+  sops.age.keyFile = "/var/lib/sops-nix/key.txt";
+  # This will generate a new key if the key specified above does not exist
+  sops.age.generateKey = true;
+}
