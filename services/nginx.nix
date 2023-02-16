5a5a3ae15aa53a0a179bcb8d91ee3265189dd650
Thijs Raymakers <thijs@raymakers.nl>
2023-02-16 22:15:59 +0100

    Really disable nginx

--- services/nginx.nix
+++ services/nginx.nix
@@ -1,23 +1,23 @@
-{ ... }: {
-  security.acme = {
-    acceptTerms = true;
-    defaults.email = "thijs@raymakers.nl";
-  };
-  services.nginx = {
-    enable = true;
-    recommendedProxySettings = true;
-    recommendedOptimisation = true;
-    recommendedGzipSettings = true;
+# { ... }: {
+#   security.acme = {
+#     acceptTerms = true;
+#     defaults.email = "thijs@raymakers.nl";
+#   };
+#   services.nginx = {
+#     enable = true;
+#     recommendedProxySettings = true;
+#     recommendedOptimisation = true;
+#     recommendedGzipSettings = true;
 
-    virtualHosts."matrix.raymakers.nl" = {
-      forceSSL = true;
-      enableACME = true;
-      http2 = true;
-      http3 = true;
-      locations."/" = {
-        proxyWebsockets = true;
-        proxyPass = "http://www.example.org/";
-      };
-    };
-  };
-}
+#     virtualHosts."matrix.raymakers.nl" = {
+#       forceSSL = true;
+#       enableACME = true;
+#       http2 = true;
+#       http3 = true;
+#       locations."/" = {
+#         proxyWebsockets = true;
+#         proxyPass = "http://www.example.org/";
+#       };
+#     };
+#   };
+# }
