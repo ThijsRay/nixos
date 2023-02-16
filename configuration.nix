08945f70b3fa7a885ddaca5198a8af4392d2851b
Thijs Raymakers <thijs@raymakers.nl>
2023-02-16 23:53:43 +0100

    Add dendrite group

--- configuration.nix
+++ configuration.nix
@@ -15,18 +15,24 @@
   networking.domain = "thijs.sh";
   services.openssh.enable = true;
   system.stateVersion = "22.11";
-  users.users = {
-		thijs = {
-			isNormalUser = true;
-			extraGroups = [
-				"wheel"
-			];
-			openssh.authorizedKeys.keys = [
-				"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLBbTukN9Wxeu6i0uNv3z0nft5MqlR+Ou+EKFpqySY9 thijs" 
-			];
-		};
-    dendrite = {
-      isSystemUser = true;
+  users = {
+    users = {
+      thijs = {
+        isNormalUser = true;
+        extraGroups = [
+          "wheel"
+        ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLBbTukN9Wxeu6i0uNv3z0nft5MqlR+Ou+EKFpqySY9 thijs" 
+        ];
+      };
+      dendrite = {
+        isSystemUser = true;
+        group = "dendrite"
+      };
+    };
+    groups = {
+      dendrite = {};
     };
   };
 }
