c41ca2b5d828ee888a6b0c2ea92a5bfc10f9ecf3
Thijs Raymakers <thijs@raymakers.nl>
2023-02-16 23:43:13 +0100

    Revert back to default acme server

--- services/nginx.nix
+++ services/nginx.nix
@@ -2,7 +2,6 @@
   security.acme = {
     acceptTerms = true;
     defaults.email = "thijs@raymakers.nl";
-    defaults.server = "https://acme-staging-v02.api.letsencrypt.org/directory";
   };
   services.nginx = {
     enable = true;
